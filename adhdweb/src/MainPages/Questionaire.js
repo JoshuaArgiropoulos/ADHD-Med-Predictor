aa04880928f5ba4e984f2440e5e2d35dc1cc9071
Zachary-Kiz <zachary.kizell@oneredmaple.com>
2024-03-09 18:43:50 -0500

    Update questionairre css

--- adhdweb/src/MainPages/Questionaire.js
+++ adhdweb/src/MainPages/Questionaire.js
@@ -7,15 +7,21 @@ import styles from '../Styles/HomePage.module.css'
 const Questionaire = () => {
   let all = ["Hello", "CHeck"];
   let colors = ["Blue", "brown", "gray"];
+  let yn = ["Yes", "No"];
   return (
     <div>
         <Header/>
 
       <Navigation />
       <div className={styles.container}>
+        <h3 id="head">
+        Please answer the following questionnaire. Using your responses, we will match you with the ADHD medication that best suits your needs.
+        </h3>
+        
         <form>
-          <QuestionWrap question="Piss" all={all} />
+          <QuestionWrap question="Test" all={all} />
           <QuestionWrap question="What color are your eyes?" all={colors}></QuestionWrap>
+          <QuestionWrap question="Do you struggle with sleep deprivation" all={yn}></QuestionWrap>
           <div style={{ textAlign: 'center'}}>
             <input className="sub" type="submit"></input>
           </div>
