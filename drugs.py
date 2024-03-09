7d1c003ab3d1d1212fbfef822ec7ecff92dd316a
Zachary-Kiz <zachary.kizell@oneredmaple.com>
2024-03-09 14:02:52 -0500

    Added list of ADHD meds

--- drugs.py
+++ drugs.py
@@ -8,6 +8,24 @@ genes = [
     "VKORC1", "CYP2C9", 
 ]
 
+adhd_drugs = [
+    "Methylphenidate", "Ritalin",
+    "Dexmethylphenidate","Focalin",
+    "Amphetamine-dextroamphetamine", "Adderall",
+    "Lisdexamfetamine", "Vyvanse",
+    "Atomoxetine", "Strattera",
+    "Guanfacine", "Intuniv",
+    "Clonidine", "Kapvay",
+    "Dextroamphetamine", "Dexedrine",
+    "Methamphetamine", "Desoxyn",
+    "Pemoline", "Cylert",
+    "Bupropion", "Wellbutrin", "Zyban",
+    "Modafinil", "Provigil",
+    "Armodafinil", "Nuvigil",
+    "Dextroamphetamine", "Amphetamine", "Evekeo",
+    "Dextroamphetamine", "Zenzedi",
+]
+
 parameters = {
     "target": "drug",
     "algorithm": "trustrank"
@@ -19,7 +37,10 @@ r = task.get_result()
 
 genes = r.get_genes()
 drugs = r.get_drugs()
-print(drugs)
+
+for drug in adhd_drugs:
+    if drug in drugs.keys():
+        print(drug)
 
 # save directly to files
 r.download_json()
